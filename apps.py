8962177679badf57b7453991930396572cbe15a7
AramKarimi <aram.karimi@gu.se>
2023-01-10 15:47:17 +0100

    create new branch for rephotography

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ExtendedRephotographyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.extended_rephotography'
