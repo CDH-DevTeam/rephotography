987530a6538231e6f9c44fd8b958efd875fe0d60
AramKarimi <aram.karimi@gu.se>
2023-03-06 17:11:13 +0100

    update view for place

--- views.py
+++ views.py
@@ -3,15 +3,33 @@ from . import models, serializers
 from django.db.models import Count, Q
 from diana.abstract.views import DynamicDepthViewSet, GeoViewSet
 from diana.abstract.models import get_fields, DEFAULT_FIELDS
+from django.contrib.postgres.aggregates import ArrayAgg
 
 
 class PlaceViewSet(DynamicDepthViewSet):
-    
-    queryset = models.Place.objects.all()
+
+    # queryset = models.Place.objects.all()
     serializer_class = serializers.PlaceSerializer
     filterset_fields = get_fields(models.Place, exclude=DEFAULT_FIELDS + ['geometry'])
     search_fields = ['placename']
-    
+
+    def get_queryset(self):
+        model_name = str(self.request.query_params.get('q'))
+        start_date = self.request.query_params.get('s')
+        end_date = self.request.query_params.get('e')
+        queryset = ''
+        if model_name in 'image':
+            queryset = models.Place.objects.all().filter(id__in=list(models.Image.objects.values_list('place', flat=True)))\
+            .filter(min_year__year= start_date)\
+            .filter(max_year__year= end_date)\
+            .filter(Q(min_year__year= start_date) & Q(max_year__year= end_date))
+        elif model_name in 'video':
+            queryset = models.Place.objects.all().filter(id__in=list(models.Video.objects.values_list('place', flat=True)))
+        elif model_name in 'observation':
+            queryset = models.Place.objects.all().filter(id__in=list(models.Observation.objects.values_list('place', flat=True)))
+        return queryset
+
+
 
 
 class PlaceGeoViewSet(GeoViewSet):
@@ -36,19 +54,10 @@ class IIIFImageViewSet(DynamicDepthViewSet):
     count:
     Returns a count of the existing images after the application of any filter.
     """
-    
     queryset = models.Image.objects.all()
     serializer_class = serializers.TIFFImageSerializer
     filterset_fields = get_fields(models.Image, exclude=DEFAULT_FIELDS + ['iiif_file', 'file'])
 
-
-    # def get_queryset(self):
-    #     queryset = models.Image.objects.annotate(
-    #         count=Count('place', distinct=True),
-    #         ).filter(count=1)
-    #     return queryset
-
-    
 
 class VideoViewSet(DynamicDepthViewSet):
     
