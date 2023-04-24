50f4462838193f787c9caaa0be164264c6b9b795
AramKarimi <aram.karimi@gu.se>
2023-04-24 13:35:02 +0200

    add id option

--- serializers.py
+++ serializers.py
@@ -9,14 +9,14 @@ class PlaceSerializer(DynamicDepthSerializer):
 
     class Meta:
         model = Place
-        fields = get_fields(Place, exclude=DEFAULT_FIELDS+['min_year', 'max_year'])
+        fields = get_fields(Place, exclude=DEFAULT_FIELDS+['min_year', 'max_year'])+ ['names', 'id']
 
 
 class PlaceGeoSerializer(GeoFeatureModelSerializer):
 
     class Meta:
         model = Place
-        fields = get_fields(Place, exclude=DEFAULT_FIELDS)
+        fields = get_fields(Place, exclude=DEFAULT_FIELDS)+ ['names', 'id']
         geo_field = 'geometry'
         depth = 1
 
@@ -25,25 +25,25 @@ class TIFFImageSerializer(DynamicDepthSerializer):
 
     class Meta:
         model = Image
-        fields = get_fields(Image, exclude=DEFAULT_FIELDS)
+        fields = get_fields(Image, exclude=DEFAULT_FIELDS)+ ['id']
 
 
 class VideoSerializer(DynamicDepthSerializer):
 
     class Meta:
         model = Video
-        fields = get_fields(Video, exclude=DEFAULT_FIELDS)
+        fields = get_fields(Video, exclude=DEFAULT_FIELDS)+ ['id']
 
 
 class ObservationSerializer(DynamicDepthSerializer):
 
     class Meta:
         model = Observation
-        fields = get_fields(Observation, exclude=DEFAULT_FIELDS)
+        fields = get_fields(Observation, exclude=DEFAULT_FIELDS)+ ['id']
 
 
 class RePhotographySerializer(DynamicDepthSerializer):
 
     class Meta:
         model = RePhotography
-        fields = get_fields(RePhotography, exclude=DEFAULT_FIELDS)
+        fields = get_fields(RePhotography, exclude=DEFAULT_FIELDS)+ ['id']
